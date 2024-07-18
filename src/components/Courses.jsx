109afb8196601eca2a0c37615d1544b1a243a430
tulnikov <dima@tulnikov.ru>
2024-07-18 16:37:47 +0300

    End Lesson
    Недоделал роутинг

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,15 +1,22 @@
 import data from '../data/courses';
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import queryString from "query-string";
+import {useState} from "react";
 
 const Courses = () => {
+    const location = useLocation();
+    const query = queryString.parse(location.search);
+
+    const [sortedCourses, setSortedCourses] = useState()
+    
     return <>
         <h1>Courses</h1>
 
-            {data.map((course) => (
-                <div key={course.id}>
-                    <h2><Link to={course.slug}>{course.title}</Link></h2>
-                </div>
-            ))}
+        {data.map((course) => (
+            <div key={course.id}>
+                <h2><Link to={course.slug}>{course.title}</Link></h2>
+            </div>
+        ))}
 
     </>
 
--- src/components/SingleCourse.jsx
+++ src/components/SingleCourse.jsx
@@ -1,16 +1,25 @@
-import {Link, useParams} from "react-router-dom";
+import {Link, useParams, useNavigate} from "react-router-dom";
+import {useEffect} from "react";
 
 import data from '../data/courses';
 
 
 const SingleCourse = () => {
     const params = useParams();
+    const navigate = useNavigate();
+
     const course = data.find(course => course.slug === params.slug);
+    useEffect(() => {
+        if (!course) {
+            navigate("..", {relative: 'path'});
+        }
+    }, [course, navigate]);
+
     return (
         <div>
-            <h1>{course.title}</h1>
-            <h2>{course.slug}</h2>
-            <h3>{course.id}</h3>
+            <h1>{course?.title}</h1>
+            <h2>{course?.slug}</h2>
+            <h3>{course?.id}</h3>
 
             <Link to='..' relative='path'>All courses</Link>
         </div>
